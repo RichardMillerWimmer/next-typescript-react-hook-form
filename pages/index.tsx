11d591e546d0cdd2a90bef53cd5fa42d0d759b47
Richard Miller <richardmmill@gmail.com>
2021-10-05 17:17:25 -0400

    setup in index for form

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+import {useForm, SubmitHandler} from 'react-hook-form';
+
+interface IFormInputs {
+  email: string,
+  password: string
+}
+
 const Home: NextPage = () => {
+  const { register, handleSubmit, watch, formState: {errors} } = useForm<IFormInputs>()
+
+  const formSubmitHandler: SubmitHandler<IFormInputs> = (data: IFormInputs) => {
+    console.log(data)
+  }
+  console.log(errors)
+  console.log(watch('email'))
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,44 +28,17 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <form onSubmit={handleSubmit(formSubmitHandler)}>
+          <input defaultValue='example@email.com' {...register('email', {required: true})} />
+          <br/>
+          {errors.email && <span>This field is required.</span>}
+          <br/>
+          <input type='password' placeholder='password' {...register('password', {required: true})} />
+          <br/>
+          {errors.password && <span>This field is required.</span>}
+          <br/>
+          <input type='submit' />
+        </form>
       </main>
 
       <footer className={styles.footer}>
